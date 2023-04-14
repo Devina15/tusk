fe8f8ab22385a4e5f984bdd1303f417ab4b5c618
Devina15 <130356637+Devina15@users.noreply.github.com>
2023-04-14 11:00:14 +0530

    storing data in local storage

--- form.js
+++ form.js
@@ -19,17 +19,22 @@ l3.addEventListener('mouseout',(e)=> {
 
 function details(formblock){
     var idn = document.getElementById("idn").value;
-    console.log("ID : "+idn)
+    console.log("ID : "+idn);
+    localStorage.setItem('id',idn);
     
     var nam = document.getElementById("name").value;
-    console.log("Name : "+nam)
+    console.log("Name : "+nam);
+    localStorage.setItem('Name',nam);
                 
     var dat = document.getElementById("dob").value;
-    console.log("D.O.B : "+dat)
+    console.log("D.O.B : "+dat);
+    localStorage.setItem('D.O.B',dat);
                 
     var mob = document.getElementById("mobile").value;
-    console.log("Mobile : "+mob)
+    console.log("Mobile : "+mob);
+    localStorage.setItem('Mobile',mob);
                 
     var mail = document.getElementById("email").value;
-    console.log("Email : "+mail)
+    console.log("Email : "+mail);
+    localStorage.setItem('Email',mail);
 }
