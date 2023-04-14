629254afcc935897591ded9ca32ff16189f1f84d
Devina15 <130356637+Devina15@users.noreply.github.com>
2023-04-14 11:45:25 +0530

    storing user data as an object in local storage

--- form.js
+++ form.js
@@ -19,22 +19,14 @@ l3.addEventListener('mouseout',(e)=> {
 
 function details(formblock){
     var idn = document.getElementById("idn").value;
-    console.log("ID : "+idn);
-    localStorage.setItem('id',idn);
-    
-    var nam = document.getElementById("name").value;
-    console.log("Name : "+nam);
-    localStorage.setItem('Name',nam);
-                
-    var dat = document.getElementById("dob").value;
-    console.log("D.O.B : "+dat);
-    localStorage.setItem('D.O.B',dat);
-                
-    var mob = document.getElementById("mobile").value;
-    console.log("Mobile : "+mob);
-    localStorage.setItem('Mobile',mob);
-                
-    var mail = document.getElementById("email").value;
-    console.log("Email : "+mail);
-    localStorage.setItem('Email',mail);
+    let obj = {
+        id:document.getElementById("idn").value,
+        name:document.getElementById("name").value,
+        dob:document.getElementById("dob").value,
+        mobile:document.getElementById("mobile").value,
+        email:document.getElementById("email").value
+    };
+    let objS = JSON.stringify(obj);
+    localStorage.setItem(idn,objS);
+    //let objD = JSON.parse(localStorage.getItem('obj'));
 }
