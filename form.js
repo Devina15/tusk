1daaf967b50c1b015cbfc67066ffa0a1cd266e54
Devina15 <130356637+Devina15@users.noreply.github.com>
2023-04-15 21:01:29 +0530

    added function to delete from UI and LS

--- form.js
+++ form.js
@@ -16,6 +16,8 @@ l3.addEventListener('mouseout',(e)=> {
     document.querySelector('#formblock').style.background = 'beige'; 
 });
 
+var userList = document.getElementById('listOfStd');
+userList.addEventListener('click',removeItem);
 
 function details(formblock){
     var idn = document.getElementById("idn").value;
@@ -29,14 +31,31 @@ function details(formblock){
     let objS = JSON.stringify(obj);
     localStorage.setItem(idn,objS);
     //let objD = JSON.parse(localStorage.getItem('obj'));
-    printOnScreen(obj);
+    printOnScreen(obj,idn);
 }
 
-function printOnScreen(obj){
+function printOnScreen(obj,idn){
     var parentElem = document.getElementById('listOfStd');
-    
     var childElem = document.createElement('li');
+    childElem.className = 'userDetails';
+    childElem.setAttribute('data-key',idn);
     childElem.textContent = obj.id+' , '+obj.name+' , '+obj.dob+' , '+obj.mobile+' , '+obj.email;
     parentElem.appendChild(childElem);
+    
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-danger delete';
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+    childElem.appendChild(deleteBtn);   
 }
 
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure?')){
+            var li = e.target.parentElement;
+            const key = e.target.parentElement.getAttribute('data-key');
+            userList.removeChild(li);
+            localStorage.removeItem(key);
+            
+            }
+    }
+}
